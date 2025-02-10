c041f421d72dc2e24887d306e5e941c6f99bf5fb
kaal3030 <kaal@flemingcollege.ca>
2025-02-09 21:58:43 -0500

    step 8

--- lab4_script_template.py
+++ lab4_script_template.py
@@ -17,6 +17,12 @@ def main():
         for ip in extracted_source_ips:
             print(ip[0])
 
+    port_counts = tally_port_traffic(log_file)
+
+    print("\nPort Traffic Tally:")
+    for port, count in sorted(port_counts.items(), key=lambda x: x[1], reverse=True):
+        print(f"Port {port}: {count} times")
+
 # Step 3
 def get_log_file_path_from_cmd_line():
     if len(sys.argv) < 2:
@@ -33,7 +39,15 @@ def get_log_file_path_from_cmd_line():
 
 # TODO: Step 8
 def tally_port_traffic(log_file):
-    return
+     port_counts = {}
+     
+     _, captured_ports = filter_log_by_regex(log_file, r"DST=(\d+)", True, False, False)
+     
+     for port_tuple in captured_ports:
+        port = port_tuple[0]
+        port_counts[port] = port_counts.get(port, 0) + 1
+    
+     return port_counts
 
 # TODO: Step 9
 def generate_port_traffic_report(log_file, port_number):
